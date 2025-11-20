8eb704490c5b0a08c31be61adb81830731b156dd
alex <907785012@qq.com>
2025-11-20 19:52:13 +0800

    format

--- 2025-winter/intro/vitest-vue-demo/tests/App.test.ts
+++ 2025-winter/intro/vitest-vue-demo/tests/App.test.ts
@@ -1,13 +1,13 @@
-import { expect, test } from "vitest";
-import App from "@/App.vue";
-import { render, screen } from "@testing-library/vue";
-import "@testing-library/jest-dom/vitest";
+import { expect, test } from 'vitest';
+import App from '@/App.vue';
+import { render, screen } from '@testing-library/vue';
+import '@testing-library/jest-dom/vitest';
 
-test("expect App component to be in the document", () => {
+test('expect App component to be in the document', () => {
   render(App);
 
-  const headingElement = screen.getByRole("heading");
-  const buttonElement = screen.getByRole("button");
+  const headingElement = screen.getByRole('heading');
+  const buttonElement = screen.getByRole('button');
 
   // expect(result).matcher
   expect(headingElement).toBeInTheDocument();
