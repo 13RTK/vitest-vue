4bef4ab6dab309fa042a9bbb7f90579b962de8a9
alex <907785012@qq.com>
2025-11-25 14:43:56 +0800

    complete section1-3(auto cleanup and beforeEach)

--- 2025-winter/part1/section3/final/tests/components/TogglePurple.test.ts
+++ 2025-winter/part1/section3/final/tests/components/TogglePurple.test.ts
@@ -1,20 +1,24 @@
 import TogglePurple from '@/components/TogglePurple.vue';
 import { render, screen } from '@testing-library/vue';
 
-render(TogglePurple);
+describe('TogglePurple render test', () => {
+  beforeEach(() => {
+    render(TogglePurple);
+  });
 
-it('should render checkbox and the checkbox not be checked by default', () => {
-  const checkbox = screen.getByRole('checkbox');
+  it('should render checkbox and the checkbox not be checked by default', () => {
+    const checkbox = screen.getByRole('checkbox');
 
-  // expect(element).matcher
-  expect(checkbox).toBeInTheDocument();
-  expect(checkbox).not.toBeChecked();
-});
+    // expect(element).matcher
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+  });
 
-it('should render purple label component with purple text', () => {
-  // const purpleLabel = screen.getByText(/purple/i);
-  // expect(element).matcher
-  // expect(purpleLabel).toBeInTheDocument();
+  it('should render purple label component with purple text', () => {
+    const purpleLabel = screen.getByText(/purple/i);
+    // expect(element).matcher
+    expect(purpleLabel).toBeInTheDocument();
 
-  screen.debug();
+    // screen.debug();
+  });
 });
