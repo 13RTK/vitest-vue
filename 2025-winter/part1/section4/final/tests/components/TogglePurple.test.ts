ab188da6a84aad948de4f5a575f35225c0ca1852
alex <907785012@qq.com>
2025-11-28 13:37:31 +0800

    add starter of section1-4

---
+++ 2025-winter/part1/section4/final/tests/components/TogglePurple.test.ts
@@ -0,0 +1,24 @@
+import TogglePurple from '@/components/TogglePurple.vue';
+import { render, screen } from '@testing-library/vue';
+
+describe('TogglePurple render test', () => {
+  beforeEach(() => {
+    render(TogglePurple);
+  });
+
+  it('should render checkbox and the checkbox not be checked by default', () => {
+    const checkbox = screen.getByRole('checkbox');
+
+    // expect(element).matcher
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+  });
+
+  it('should render purple label component with purple text', () => {
+    const purpleLabel = screen.getByText(/purple/i);
+    // expect(element).matcher
+    expect(purpleLabel).toBeInTheDocument();
+
+    // screen.debug();
+  });
+});
