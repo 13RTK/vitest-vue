8c6fbfc8e2ea6d46817dff89ae10c0774e71d464
alex <907785012@qq.com>
2025-12-02 19:34:12 +0800

    add starter of section1-7, ignore coverage

---
+++ 2025-winter/part1/section7/starter/src/components/TogglePurple.vue
@@ -0,0 +1,12 @@
+<template>
+  <label>
+    Purple
+    <input type="checkbox" v-model="isPurple" />
+  </label>
+</template>
+
+<script setup lang="ts">
+const isPurple = defineModel('isPurple', {
+  type: Boolean,
+});
+</script>
