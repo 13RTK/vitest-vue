b30ea0c4ab85778f9171b931aed52afc6d4efebc
alex <907785012@qq.com>
2025-12-06 17:34:46 +0800

    add starter of section1-8(keyboard)

---
+++ 2025-winter/part1/section8/final/src/components/SelectColor.vue
@@ -0,0 +1,16 @@
+<template>
+  <label>
+    text color
+    <select v-model="textColor">
+      <option value="" selected>White</option>
+      <option value="text-black">Black</option>
+      <option value="text-orange">Orange</option>
+    </select>
+  </label>
+</template>
+
+<script setup lang="ts">
+const textColor = defineModel('textColor', {
+  type: String,
+});
+</script>
