2405ed55d45187c9006f521bfd3c572240d4dc18
alex <907785012@qq.com>
2025-12-11 17:53:58 +0800

    add starter of section1-10

---
+++ 2025-winter/part1/section10/final/src/components/CircleProperty.vue
@@ -0,0 +1,12 @@
+<template>
+  <label>
+    <slot />
+    <input type="number" v-model="property" />
+  </label>
+</template>
+
+<script setup lang="ts">
+const property = defineModel('property', {
+  type: Number,
+});
+</script>
