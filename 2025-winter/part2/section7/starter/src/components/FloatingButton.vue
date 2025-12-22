3a798f4e7b17a44db6f162429a60f204295c0773
alex <907785012@qq.com>
2025-12-22 22:45:25 +0800

    add starter of section2-7

---
+++ 2025-winter/part2/section7/starter/src/components/FloatingButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    @click="openCreateModal"
+    class="btn btn-primary btn-circle btn-lg fixed bottom-6 right-6 shadow-xl z-50"
+  >
+    <PhPlus size="{52}" weight="thin" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { openModal } from '@/composables/model';
+import { useNoteStore } from '@/stores/note';
+import type { Note } from '@/types/Note';
+import { storeToRefs } from 'pinia';
+import { PhPlus } from '@phosphor-icons/vue';
+
+const { note } = storeToRefs(useNoteStore());
+
+function openCreateModal() {
+  // Set default values for a new note
+  note.value = {
+    id: 999,
+    title: 'Example Title',
+    content: 'Example Content',
+  } as Note;
+  openModal('my_modal_5');
+}
+</script>
