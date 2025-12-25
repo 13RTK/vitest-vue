8d8ff1616aef98c5bfe8220aacdca1621ba532cb
alex <907785012@qq.com>
2025-12-25 16:32:05 +0800

    add starter of section3-1

---
+++ 2025-winter/part3/section1/starter/src/components/NoteListSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    role="progressbar"
+    class="flex w-full flex-col gap-4 mt-8 mx-auto items-center"
+  >
+    <div class="skeleton h-8 w-4/5"></div>
+    <div class="skeleton h-8 w-4/5"></div>
+    <div class="skeleton h-8 w-4/5"></div>
+  </div>
+</template>
---
+++ 2025-winter/part3/section1/starter/tests/components/NoteList.test.ts
@@ -0,0 +1,79 @@
+import NoteList from '@/components/NoteList.vue';
+import { getNotes } from '@/services/apiNote';
+import type { Note } from '@/types/Note';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import { createPinia } from 'pinia';
+import { render } from 'vitest-browser-vue';
+
+vi.mock('@/services/apiNote');
+
+describe('NoteList', () => {
+  function renderComponent() {
+    const vueQueryPluginOptions = {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      },
+    };
+
+    return render(NoteList, {
+      global: {
+        plugins: [createPinia(), [VueQueryPlugin, vueQueryPluginOptions]],
+      },
+    });
+  }
+
+  describe('render test', () => {
+    it('should render note items with correct count', async () => {
+      // Arrange
+      const mockNotes: Note[] = [
+        {
+          id: 1,
+          title: 'title',
+          content: 'content',
+        },
+        {
+          id: 2,
+          title: 'title',
+          content: 'content',
+        },
+      ];
+      vi.mocked(getNotes).mockResolvedValue(mockNotes);
+      const { getByRole } = renderComponent();
+
+      // Act
+      await vi.waitFor(() => {
+        const listitems = getByRole('listitem').all();
+        expect(listitems.length).toBeGreaterThan(0);
+      });
+      const noteItems = getByRole('listitem').elements();
+
+      // Assert
+      expect(noteItems).toHaveLength(mockNotes.length);
+    });
+
+    it('should render the skeleton while data is fetching', async () => {
+      vi.mocked(getNotes).mockImplementation(() => {
+        return new Promise(() => {});
+      });
+      const { getByRole } = renderComponent();
+
+      const skeleton = getByRole('progressbar');
+
+      await expect.element(skeleton).toBeInTheDocument();
+    });
+
+    it('should display error message while fetch function throw error', async () => {
+      const errorMessage = 'Error from vitest';
+      vi.mocked(getNotes).mockRejectedValue(new Error(errorMessage));
+      const { getByRole } = renderComponent();
+
+      const alert = getByRole('alert');
+
+      await expect.element(alert).toBeInTheDocument();
+    });
+  });
+});
